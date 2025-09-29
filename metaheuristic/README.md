651fa7dcf66010d229a2f85f37341d681d569398
jspast <140563347+jspast@users.noreply.github.com>
2025-09-29 17:22:17 -0300

    metaheuristic: Initial commit

--- metaheuristic/README.md
+++ metaheuristic/README.md
@@ -1,1 +1,26 @@
 # Meta-heurística GRASP para o problema da peregrinação
+
+## Compilação e Execução
+
+Esse projeto faz uso do sistema de compilação [Meson](https://mesonbuild.com/).
+Veja o [guia de uso do Meson](https://mesonbuild.com/Quick-guide.html) para informações gerais sobre como obter e usar o Meson.
+Também é necessária a instalação de um compilador e biblioteca padrão de C++17.
+
+1. Inicialize o diretório do sistema de compilação (no exemplo, `build`):
+
+```shell
+meson setup build
+```
+
+2. Compile o projeto neste diretório:
+
+```shell
+meson compile -C build
+```
+
+3. Execute o programa gerado:
+
+```shell
+./build/grasper [file] [max_iterations] [seed]
+```
+
---
+++ metaheuristic/main.cpp
@@ -0,0 +1,54 @@
+#include <cmath>
+#include <iostream>
+#include <string_view>
+
+struct temple {
+    uint x;
+    uint y;
+};
+
+struct dependency {
+    uint a;
+    uint b;
+};
+
+struct problem {
+    uint num_temples;
+    struct temple *temples;
+    uint num_dependencies;
+    struct dependency *dependencies;
+};
+
+int temples_distance(struct temple a, struct temple b)
+{
+    return std::sqrt((float)((b.x - a.x)*(b.x - a.x) + (b.y - a.y)*(b.y - a.y))) * 100;
+}
+
+void print_solution()
+{
+
+}
+
+void print_help()
+{
+    std::cout << "GRASPer peregrinação solver using GRASP\n";
+    std::cout << "usage: grasper [file] [max_iterations] [seed]\n";
+}
+
+int main(int argc, char *argv[])
+{
+    std::string_view input_path;
+    uint max_iterations;
+    uint seed;
+
+    if (argc < 4) {
+        print_help();
+        return 0;
+    }
+
+    input_path = argv[1];
+    max_iterations = std::atoi(argv[2]);
+    seed = std::atoi(argv[3]);
+
+    return 0;
+}
