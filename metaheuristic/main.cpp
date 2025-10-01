dbcae5bbefa0cc76682b9c9691a971f21cac910a
jspast <140563347+jspast@users.noreply.github.com>
2025-10-01 00:17:22 -0300

    metaheuristic: Implement alpha term with RCL

--- metaheuristic/main.cpp
+++ metaheuristic/main.cpp
@@ -10,6 +10,8 @@
 
 using uint = unsigned int;
 
+#define DEFAULT_ALPHA 0.1
+
 struct temple {
     uint x;
     uint y;
@@ -27,7 +29,7 @@ struct solution {
     uint *route;
 };
 
-struct neighbour {
+struct candidate {
     uint idx;
     uint distance;
 };
@@ -67,15 +69,15 @@ const struct problem& parse_input(std::ifstream file)
 
 uint temples_distance(const struct temple& a, const struct temple& b)
 {
-    return std::sqrt((float)((b.x - a.x)*(b.x - a.x) + (b.y - a.y)*(b.y - a.y))) * 100;
+    return std::sqrt((double)((b.x - a.x)*(b.x - a.x) + (b.y - a.y)*(b.y - a.y))) * 100;
 }
 
 uint compute_solution_value(const struct problem& prob, const struct solution& sol)
 {
     uint value = 0;
 
-    for (uint i = 0; i+1 < prob.num_temples; ++i) {
-        value += temples_distance(prob.temples[sol.route[i]], prob.temples[sol.route[i+1]]);
+    for (uint i = 0; i + 1 < prob.num_temples; ++i) {
+        value += temples_distance(prob.temples[sol.route[i]], prob.temples[sol.route[i + 1]]);
     }
 
     return value;
@@ -91,19 +93,42 @@ void print_solution(const struct solution& sol, const uint sol_size)
     std::cout << sol.route[sol_size - 1] + 1 << '\n';
 }
 
-// For debbuging purposes, prints <idx>:<distance> for each neighbour
-void print_neighbourhood(std::vector<struct neighbour> neighbourhood)
-{
-    for (auto n : neighbourhood) {
-        std::cout << n.idx << ':' << n.distance << ' ';
-    }
-    std::cout << '\n';
-}
-
-// Sorts neighbours by the distance
-bool neighbour_sorter(const struct neighbour& l, const struct neighbour& r)
+// Sorts candidates by the distance
+bool candidate_sorter(const struct candidate& l, const struct candidate& r)
 {
     return l.distance < r.distance;
+}
+
+// Builds a Restrictive Candidate List based on alpha
+std::vector<struct candidate> build_rcl(
+    const struct problem prob,
+    const std::set<uint>& candidates,
+    uint last_chosen,
+    double alpha)
+{
+    std::vector<struct candidate> candidates_distances;
+
+    // For each candidate, compute the distance to the last temple
+    for (uint c : candidates) {
+        candidates_distances.push_back({c,
+            temples_distance(prob.temples[last_chosen], prob.temples[c])
+        });
+    }
+
+    auto [min_it, max_it] = std::minmax_element(
+        candidates_distances.begin(), candidates_distances.end(), &candidate_sorter);
+
+    double min = min_it->distance;
+    double max = max_it->distance;
+    double threshold = min + alpha * (max - min);
+
+    std::vector<struct candidate> rcl;
+    for (const auto& c : candidates_distances) {
+        if (c.distance <= threshold)
+            rcl.push_back(c);
+    }
+
+    return rcl;
 }
 
 // Builds a greedy_randomized solution for the problem
@@ -111,72 +136,68 @@ bool neighbour_sorter(const struct neighbour& l, const struct neighbour& r)
 void greedy_randomized(
     struct problem& prob,
     struct solution& sol,
-    float alpha,
-    uint k,
-    std::default_random_engine rng)
+    double alpha,
+    std::mt19937& rng)
 {
     sol.value = 0;
 
-    std::vector<struct neighbour> neighbourhood;
+    // Use vector for the first choice as it is better for selecting a random element
+    std::vector<uint> first_candidates;
+    std::set<uint> candidates;
 
     for (uint i = 0; i < prob.num_temples; ++i) {
-        if (prob.temples[i].prerequisites.empty())
-            neighbourhood.push_back({i, 0});
+        if (prob.temples[i].prerequisites.empty()) {
+            candidates.insert(i);
+            first_candidates.push_back(i);
+        }
     }
 
-    // The first temple should be chosen randomly
-    std::uniform_int_distribution<> dist(0, neighbourhood.size());
-    uint chosen = dist(rng);
+    // The first chosen temple is completely random
+    std::uniform_int_distribution<> dist(0, first_candidates.size() - 1);
+    struct candidate chosen = {first_candidates[dist(rng)], 0};
+    struct candidate prev_chosen = chosen;
 
-    for (uint sol_size = 0; sol_size < prob.num_temples; sol_size++) {
+    for (uint sol_size = 0; sol_size < prob.num_temples - 1; sol_size++) {
 
-        // Debugging:
-        // print_neighbourhood(neighbourhood);
-        // std::cout << neighbourhood[chosen].idx << '\t'
-        //           << prob.temples[neighbourhood[chosen].idx].x << '\t'
-        //           << neighbourhood[chosen].distance << "\t| "
-        //           << chosen << '\t' << neighbourhood.size() << '\n';
-
-        uint cur_temple_idx = neighbourhood[chosen].idx;
-        sol.value += neighbourhood[chosen].distance;
-        sol.route[sol_size] = cur_temple_idx;
+        sol.value += chosen.distance;
+        sol.route[sol_size] = chosen.idx;
 
         // Remove the prerequisite from other temples
-        for (auto i : prob.temples[cur_temple_idx].dependents) {
-            prob.temples[i].prerequisites.erase(cur_temple_idx);
+        for (auto i : prob.temples[chosen.idx].dependents) {
+            prob.temples[i].prerequisites.erase(chosen.idx);
 
-            // Bring temple to the neighbourhood if there is no prerequisite anymore
+            // Add temple to the candidates if there is no prerequisite anymore
             if (prob.temples[i].prerequisites.empty())
-                neighbourhood.push_back({i, 0});
+                candidates.insert(i);
         }
 
-        // For each neighbour, update the distance to the current temple
-        for (uint i= 0; i < neighbourhood.size(); ++i) {
-            neighbourhood[i].distance = temples_distance(
-                prob.temples[cur_temple_idx],
-                prob.temples[neighbourhood[i].idx]
-            );
-        }
+        // Remove the current temple from the candidates
+        candidates.erase(chosen.idx);
+        prev_chosen = chosen;
 
-        // Remove the current temple from the neighbourhood
-        neighbourhood.erase(neighbourhood.begin() + chosen);
-
-        // Sort neighbours by the distance
-        std::sort(neighbourhood.begin(), neighbourhood.end(), &neighbour_sorter);
-
-        // Choose the next temple from the neighbourhood
-        // TODO: implement alpha term
-        std::uniform_int_distribution<> dist(0, std::min((uint)neighbourhood.size() -1, k -1));
-        chosen = dist(rng);
+        // Choose the next temple with a Restrictive Candidate List
+        std::vector<struct candidate> rcl = build_rcl(prob, candidates, chosen.idx, alpha);
+        std::uniform_int_distribution<> dist(0, rcl.size() - 1);
+        chosen = rcl[dist(rng)];
     }
+
+    sol.value += chosen.distance;
+    sol.route[prob.num_temples - 1] = chosen.idx;
+}
+
+// Copy problem a to b, which must have already been allocated
+void copy_problem(const struct problem& a, struct problem& b)
+{
+    b.num_temples = a.num_temples;
+    for (uint i = 0; i < a.num_temples; ++i)
+        b.temples[i] = a.temples[i];
 }
 
 struct solution& grasp(
     const struct problem& prob,
     uint num_iterations,
-    float alpha,
-    uint k,
-    std::default_random_engine rng)
+    double alpha,
+    std::mt19937& rng)
 {
     struct solution *sol = new struct solution;
     sol->route = new uint[prob.num_temples]{0};
@@ -185,11 +206,13 @@ struct solution& grasp(
     best_sol->route = new uint[prob.num_temples]{0};
     best_sol->value = std::numeric_limits<uint>::max();
 
-    struct problem prob_tmp = prob;
+    struct problem prob_tmp;
+    prob_tmp.temples = new struct temple[prob.num_temples];
+    copy_problem(prob, prob_tmp);
 
     for (uint i = 0; i < num_iterations; ++i) {
-        greedy_randomized(prob_tmp, *sol, alpha, k, rng);
-        prob_tmp = prob;
+        greedy_randomized(prob_tmp, *sol, alpha, rng);
+        copy_problem(prob, prob_tmp);
 
         // TODO: local search
 
@@ -216,13 +239,17 @@ int main(int argc, char *argv[])
     }
 
     input_path = argv[1];
+
     num_iterations = std::atoi(argv[2]);
+
     seed = std::atoi(argv[3]);
-    std::default_random_engine rng(seed);
+    std::mt19937 rng(seed);
+
+    double alpha = argc > 4 ? std::atof(argv[4]) : DEFAULT_ALPHA;
 
     const struct problem prob = parse_input(std::ifstream(input_path.data()));
 
-    grasp(prob, num_iterations, 1, 3, rng);
+    grasp(prob, num_iterations, alpha, rng);
 
     return 0;
 }
